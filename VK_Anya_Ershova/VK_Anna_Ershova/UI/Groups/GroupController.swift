abfb71a9b6aec1ed76353f09a0b4f1d9770c0834
Anna Ershova <anechka_ershova@macbook-air-anna.local>
2019-02-20 01:00:43 +0000

    edit groups action

--- VK_Anya_Ershova/VK_Anna_Ershova/UI/Groups/GroupController.swift
+++ VK_Anya_Ershova/VK_Anna_Ershova/UI/Groups/GroupController.swift
@@ -20,51 +20,6 @@ class GroupController: UITableViewController {
     var groupService = VKService()
     var groupname = [String]()
     
-//    @IBAction func add(segue: UIStoryboardSegue) {
-//        if segue.identifier == "add" {
-//
-//            let allGroupController = segue.source as! AllGroupController
-//            if let indexPath = allGroupController.tableView.indexPathForSelectedRow {
-//
-//
-//                let gr = allGroupController.allgroupsVK[indexPath.row]
-//                if  groupsVK[indexPath.row].name.contains(gr.name) {
-//                    //groupService.addGroups(groupId: gr.id)
-//                    print("there could be error if you are member")
-//                    groupsVK.append(allGroupController.filterGr[indexPath.row])
-//
-//                    tableView.reloadData()
-//                } else {
-//                    print("error")
-//                }
-//
-//            }
-//
-//        }
-
-//    }
-//
-//    @IBAction func add(segue: UIStoryboardSegue) {
-//        if segue.identifier == "add" {
-//
-//            let allGroupController = segue.source as! AllGroupController
-//            if let indexPath = allGroupController.tableView.indexPathForSelectedRow {
-//
-//                let gr = allGroupController.filterGr[indexPath.row].id
-//                //let grAll = allGroupController.groupsAll[indexPath.row]
-//                if  groupsVK[indexPath.row].id == gr {
-//                    groupService.addGroups(gr)
-//                    //groupsVK.append(allGroupController.filterGr[indexPath.row])
-//                    tableView.reloadData()
-//                }
-//
-//            }
-//
-//        }
-//
-//    }
-
-
     
     
     override func viewDidLoad() {
@@ -75,52 +30,50 @@ class GroupController: UITableViewController {
                 return
             } else if let groupsVK = groupsVK, let self = self {
                 self.groupsVK = groupsVK
-        
+                
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
                 }
             }
         }
-
+        
+        //self.tableView.reloadData()
+        
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return groupsVK.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "GroupCell", for: indexPath) as! GroupCell
-    
-//        let group = groups[indexPath.row]
-//        let img = UIImage(named: group)
-//        cell.configure(friend: group, img: img!)
+        
         cell.configured(with: groupsVK[indexPath.row])
-
-
+        
         return cell
     }
     
-
-
+    
+    
     
     // Override to support editing the table view.
-//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        if editingStyle == .delete {
-//            groupsVK.remove(at: indexPath.row)
-//            //groupService.leftGroups(for: groupsVK[indexPath.row].id)
-//            tableView.deleteRows(at: [indexPath], with: .fade)
-//            tableView.reloadData()
-//        }
-//    }
+    //    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    //        if editingStyle == .delete {
+    //            groupsVK.remove(at: indexPath.row)
+    //            //groupService.leftGroups(for: groupsVK[indexPath.row].id)
+    //            tableView.deleteRows(at: [indexPath], with: .fade)
+    //            tableView.reloadData()
+    //        }
+    //    }
     override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
         let ownerGroup = self.groupsVK[indexPath.row]
         let delAction = UITableViewRowAction(style: .destructive, title: "Delete") { (action, indexpath) in
@@ -131,8 +84,26 @@ class GroupController: UITableViewController {
         }
         delAction.backgroundColor = .red
         self.groupService.leftGroups(for: ownerGroup.id)
+        self.tableView.reloadData()
         return [delAction]
     }
-
-
+    
+    //    @IBAction func add(segue: UIStoryboardSegue) {
+    //        if segue.identifier == "add" {
+    //
+    //            let allGroupController = segue.source as! AllGroupController
+    //            if let indexPath = allGroupController.tableView.indexPathForSelectedRow {
+    //                let gr = allGroupController.allgroupsVK[indexPath.row]
+    //                if  groupsVK[indexPath.row].name.contains(gr.name) {
+    //                    groupsVK.append(allGroupController.filterGr[indexPath.row])
+    //                    tableView.reloadData()
+    //                } else {
+    //                    print("error")
+    //                }
+    //            }
+    //        }
+    //    }
+    
+    
+    
 }
